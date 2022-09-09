c910aa50334807a2c141c50ba9ff6f02578e47fa
William Carvalho <williamcoelho.noorden@sulamerica.com.br>
2022-09-08 21:10:00 -0300

    feat:criando o projeto

---
+++ backend/projetoreact/src/test/java/com/acwilliam/projetoreact/ProjetoreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acwilliam.projetoreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
